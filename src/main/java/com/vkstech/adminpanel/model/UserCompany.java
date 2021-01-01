a5fe7a236b3e29aec487955d8108185b9bb59100
vipul-kumar-singh <vipul24.9669@gmail.com>
2021-01-01 17:16:23 +0530

    Removed Lombok Annotation

--- src/main/java/com/vkstech/adminpanel/model/UserCompany.java
+++ src/main/java/com/vkstech/adminpanel/model/UserCompany.java
@@ -1,14 +1,11 @@
 package com.vkstech.adminpanel.model;
 
-import lombok.Data;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-@Data
 public class UserCompany {
 
     @Id
@@ -18,4 +15,28 @@ public class UserCompany {
     private Integer userId;
 
     private Integer companyId;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(Integer companyId) {
+        this.companyId = companyId;
+    }
 }
--- src/main/java/com/vkstech/adminpanel/model/user/User.java
+++ src/main/java/com/vkstech/adminpanel/model/user/User.java
@@ -1,14 +1,12 @@
 package com.vkstech.adminpanel.model.user;
 
 import com.vkstech.adminpanel.annotation.EnableAdmin;
-import lombok.Data;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Data
 @Entity
 @EnableAdmin
 public class User {
@@ -21,4 +19,27 @@ public class User {
 
     private Integer age;
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 }
